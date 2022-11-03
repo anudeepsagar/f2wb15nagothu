1391014f3fb8b522d77fc675886c780fa566b685
anudeepsagar <68767594+anudeepsagar@users.noreply.github.com>
2022-11-02 22:01:13 -0500

    updated the file

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var elephantRouter = require('./routes/elephant');
-
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 
 
@@ -26,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/elephant', elephantRouter);
-
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
